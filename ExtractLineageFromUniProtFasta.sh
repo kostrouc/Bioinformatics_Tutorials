9ce1e2a7386cfcd64cee2dfedb6bdcbe8845c66e
Katherine Ostrouchov <69212968+kostrouc@users.noreply.github.com>
2022-04-05 12:54:40 -0400

    Update ExtractLineageFromUniProtFasta.sh

--- ExtractLineageFromUniProtFasta.sh
+++ ExtractLineageFromUniProtFasta.sh
@@ -3,5 +3,5 @@ LALA=/data/databases/lineage/entry.txt
 while read it; do
 perl /data/databases/lineage/entrylin.pl $it
 done <$LALA
-#The UniProt ID was extracted from the headers for each protein in the entrapmentC98 fasta using the following bash code.
+#The UniProt ID was extracted from the headers for each protein in a fasta file using the following bash code.
 awk -F '|' '/^>/ {printf(">%s\n",$2);}' entrapmentC98 | cut -c 2- > entry.txt
